80c12fa0af1a1b880a898aa9140de7f211afb018
kimjeongbae <gprhf651@naver.com>
2024-01-04 15:40:01 +0900

    end

--- src/main/java/com/example/atest/HomeController.java
+++ src/main/java/com/example/atest/HomeController.java
@@ -1,4 +1,13 @@
 package com.example.atest;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    public String root () {
+        return "redirect:/article/list";
+    }
+
 }
---
+++ src/main/java/com/example/atest/article/ArticleController.java
@@ -0,0 +1,43 @@
+package com.example.atest.article;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+@RequestMapping("/article")
+@RequiredArgsConstructor
+@Controller
+public class ArticleController {
+
+    private final ArticleService articleService;
+
+    @GetMapping("/list")
+    public String list(Model model){
+        List<Article> articleList = this.articleService.getList();
+        model.addAttribute("articleList",articleList);
+        return "article_list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Article article = this.articleService.getArticle(id);
+        model.addAttribute("article",article);
+        return "article_detail";
+    }
+
+    @GetMapping("/create")
+    public String articleCreate() {
+        return "article_form";
+    }
+
+    @PostMapping("/create")
+    public String articleCreate(@RequestParam("title") String title, @RequestParam("content") String content) {
+        this.articleService.create(title,content);
+        return "redirect:/article/list";
+    }
+
+
+}
